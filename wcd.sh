d7e53a80b1f863f3ce9975c5472e90fa55ea5225
Jari Aalto <jari.aalto@cante.net>
2009-10-22 14:09:13 +0300

    wcd.sh: adjust comments

--- wcd.sh
+++ wcd.sh
@@ -2,10 +2,10 @@
 #  Copyright (C) 2004-2009 Jari Aalto; Licensed under GPL v2 or later
 #
 #  Define a shell alias for wcd. wcd(1) cannot itself change
-#  directories, so it must be done in shell script.
+#  directories, so it must be done in a shell alias.
 #
-#  This file should be source's from ~/.bashrc placed in
-#  /etc/profile.d/
+#  This file is in /etc/profile.d/ and it should be sourced from
+#  a shell startup file: ~/.<shell>rc
 
 xxtmp=/usr/lib/wcd/wcd
 
@@ -17,6 +17,7 @@ then
 	[ -d "$HOME/.wcd/bin" ] || mkdir -p "$HOME/.wcd/bin"
 
 	go="$HOME/.wcd/bin/wcd.go"
+
 	rm -f "$go" 2> /dev/null
 
 	/usr/lib/wcd/wcd "$@"
