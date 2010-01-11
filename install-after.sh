126164eb5fa0eff87e56950287bd205a25df70ad
Jari Aalto <jari.aalto@cante.net>
2010-01-11 17:13:45 +0200

    install-after.sh: move man1 to man7

--- install-after.sh
+++ install-after.sh
@@ -25,6 +25,22 @@ Main()
 
         Cmd install -m 755 -d $dir
         Cmd install -m 755 CYGWIN-PATCHES/wcd.sh $dir
+        Cmd install -m 755 CYGWIN-PATCHES/wcd.csh $dir
+
+        # Change manual section to 7 (library)
+
+	mandir=$root/usr/share/man/man1
+
+	echo ">> Change manpage content: section 1 to 7 (library)"
+
+        Cmd mv $mandir/wcd.1 $mandir/wcd.7
+
+        Cmd sed --in-place \
+                -e 's/TH wcd 1/TH wcd 7/' \
+                $mandir/wcd.7
+
+	echo ">> Change dir structure: section 1 to 7 (library)"
+	Cmd mv $root/usr/share/man/man1 $root/usr/share/man/man7
 
     fi
 }
